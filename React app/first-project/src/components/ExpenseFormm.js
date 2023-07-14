d227af102ff79e92d99b9cbdf67d7d112ff2f512
Rii-C <rrishi535@gmail.com>
2023-07-14 17:46:16 +0530

    done

--- React app/first-project/src/components/ExpenseFormm.js
+++ React app/first-project/src/components/ExpenseFormm.js
@@ -2,9 +2,21 @@ import React, { useState } from 'react'
 
 const ExpenseFormm = () => {
 
-    const [enteredTitle , setEnteredTitle]= useState('')
-    const [enteredAmount , setEnteredAmount]= useState('')
-    const [enteredDate , setEnteredDate]= useState('')
+    // const [enteredTitle , setEnteredTitle]= useState('')
+    // const [enteredAmount , setEnteredAmount]= useState('')
+    // const [enteredDate , setEnteredDate]= useState('')
+    const [userinput , setUserInput] = useState({
+        enteredTitle : '',
+        enteredAmount : '',
+        enteredDate : ''
+    })
+
+    setUserInput({
+        ...userinput,
+        enteredTitle : Event.target.value
+    })
+
+
 
     
     
@@ -12,27 +24,44 @@ const ExpenseFormm = () => {
 
     const handler = (event) =>{
         event.preventDefault();
-        console.log("Title :" , enteredTitle , "Amount :", enteredAmount , "date :" , enteredDate)
+        
+    const ExpenseData = {
+        Title : enteredTitle,
+        Amount : enteredAmount,
+        Date : new Date(enteredDate)
     }
+
+    console.log(ExpenseData);
+        // console.log("Title :" , enteredTitle , "Amount :", enteredAmount , "date :" , enteredDate)
+    }
+
   return (
     <form>
         <div className='New-expense__constrol'>
         <div className='New-expense__constrol'>
             <lable>Title</lable>
             <input type='text' id='Text' onChange={(e)=>{
-                setEnteredTitle(e.target.value)
+                 setUserInput({
+                    ...userinput,
+                    enteredTitle : e.target.value
+                })
             }}/>
         </div>
         <div className='New-expense__constrol'>
             <lable>Amount</lable>
             <input type='number' min="0.01" step="0.01" id='amount' onChange={(e)=>{
-setEnteredAmount(e.target.value)
-            }}/>
+ setUserInput({
+    ...userinput,
+    enteredAmount: e.target.value
+})            }}/>
         </div>
         <div className='New-expense__constrol'>
             <lable>Date</lable>
             <input type='date' min="2021-01-01" step="2022-12-31" id='date' onChange={(e)=>{
-                setEnteredDate(e.target.value)
+                 setUserInput({
+                    ...userinput,
+                    enteredDate: e.target.value
+                })
             }}/>
         </div>
         <div className='New-expense__constrol'>
